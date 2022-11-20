dfcbc287d8ee5efb571ce557f11a57948110dfb8
kgsMax <kgssks@gmail.com>
2022-11-20 13:54:24 +0900

    index 수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/g', function(req,res,next){
+  res.send("hello!!");
+  console.log("call G")
+})
 
 module.exports = router;
